9bb6bd91f29557a3416b2d861f3bcb20871941e4
James Grieve <pocojames@hotmail.com>
2021-03-13 15:19:14 -0800

    configured react for frontend

---
+++ app/javascript/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default () => (
+  <div>
+      React Configured!
+        <Link
+          to="/"
+        >
+          Home
+        </Link>
+  </div>
+);
