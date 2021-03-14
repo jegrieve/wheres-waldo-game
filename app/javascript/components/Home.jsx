59a753e5bc46b0eb7031fa8ff8625ff463c467b4
James Grieve <pocojames@hotmail.com>
2021-03-14 00:57:34 -0800

    add basic react component files instructions and leaderboards

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+
 export default () => (
-  <div>
-      React Configured!
-        <Link
-          to="/"
-        >
-          Home
-        </Link>
+  <div className = "container-fluid">
+      <div className = "row d-flex">
+          <div className = "col-6 d-flex justify-content-center">
+                <div className = "leaderboard">
+                    Leaderboards
+                    {/* <Leaderboards /> */}
+                </div>
+          </div>
+          <div className = "col-6 d-flex justify-content-center flex-column">
+                <div className = "instructions">
+                    Instructions
+                    {/* <Instructions /> */}
+                </div>
+                <div className = "btn btn-success">Play!</div>
+          </div>
+      </div>
   </div>
 );
